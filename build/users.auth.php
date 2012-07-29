7cf6b80bc2e8f5c13036b0bd48d3b8fb945f4c18
Puzzlet Chung <puzzlet@gmail.com>
2012-07-29 12:59:10 +0900

    Portable DokuWiki for Windows

---
+++ build/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+admin:21232f297a57a5a743894a0e4a801fc3:Administrator:admin@example.com:admin,user
---
+++ make-win.py
@@ -0,0 +1,102 @@
+import glob
+import hashlib
+import logging
+import os
+import subprocess
+import urllib.request
+
+BASE_DIR = os.path.dirname(__file__)  # XXX abspath
+TARGET_DIR = os.path.join(BASE_DIR, 'build', 'Another Springnote')
+# GNU cp doesn't recognize forward slash
+BASE_DIR_ = BASE_DIR.replace('\\', '/')
+TARGET_DIR_ = TARGET_DIR.replace('\\', '/')
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    file_name = os.path.join(TARGET_DIR, 'stop_server.bat')
+    if os.access(file_name, os.F_OK):
+        logging.info('Stopping (possibly running) server...')
+        subprocess.call(file_name, shell=True)
+
+    if os.access(TARGET_DIR, os.F_OK):
+        cmd(['rm', '-rf', TARGET_DIR])
+    os.makedirs(TARGET_DIR, exist_ok=True)
+
+    logging.info('Exporting git repositories...')
+
+    # WNMP
+    git_pipe = subprocess.Popen(['git',
+            '--git-dir={}'.format(
+                os.path.join(BASE_DIR, '.git', 'modules', 'wnmp')),
+            '--work-tree={}'.format(os.path.join(BASE_DIR, 'wnmp')),
+            'archive', 'master'],
+        stdout=subprocess.PIPE)
+    subprocess.Popen(['tar', '-x', '-C', TARGET_DIR],
+        stdin=git_pipe.stdout).communicate()
+
+    # DokuWiki
+    os.makedirs(os.path.join(TARGET_DIR, 'nginx', 'www'), exist_ok=True)
+    git_pipe = subprocess.Popen(['git',
+            '--git-dir={}'.format(
+                os.path.join(BASE_DIR, '.git', 'modules', 'dokuwiki')),
+            '--work-tree={}'.format(os.path.join(BASE_DIR, 'dokuwiki')),
+            'archive', 'master'],
+        stdout=subprocess.PIPE)
+    subprocess.Popen(['tar', '-x', '-C', '{}/nginx/www'.format(TARGET_DIR_)],
+        stdin=git_pipe.stdout).communicate()
+
+    # PHP
+    wget('http://windows.php.net/downloads/releases/'
+            'php-5.4.5-Win32-VC9-x86.zip',
+        sha1='028eb12e09fe011e20097c82064d6c550bf896c4')
+    os.makedirs(os.path.join('tmp', 'php'), exist_ok=True)
+    cmd(['unzip', '-oq', '-d', os.path.join('tmp', 'php'),
+        'php-5.4.5-Win32-VC9-x86.zip'])
+    cmd(['bash', '-c', "cp -r tmp/php/* '{}/php/'".format(TARGET_DIR_)])
+
+    # nginx
+    wget('http://nginx.org/download/nginx-1.2.2.zip',
+        sha1='0a5dfbb766bfefa238207db25d7b64b69aa37908')
+    os.makedirs(os.path.join('tmp', 'nginx'), exist_ok=True)
+    cmd(['unzip', '-oq', '-d', os.path.join('tmp', 'nginx'),
+        'nginx-1.2.2.zip'])
+    cmd(['bash', '-c',
+        "cp -r tmp/nginx/nginx-1.2.2/* '{}/nginx/'".format(TARGET_DIR_)])
+
+    cmd(['cp', '{}/example.nginx.conf'.format(TARGET_DIR_),
+        '{}/nginx/conf/nginx.conf'.format(TARGET_DIR_)])
+    for file_name in ['acl.auth.php', 'local.php', 'users.auth.php']:
+        cmd(['cp',
+            os.path.join(BASE_DIR_, 'build', file_name).replace('\\', '/'),
+            # XXX
+            '{}/nginx/www/conf'.format(TARGET_DIR_)])
+
+    for pattern in [
+            'example.nginx.conf',
+            'readme.txt',
+            'nginx/www/_*',
+            'www/',]:
+        for file_path in glob.glob(os.path.join(TARGET_DIR,
+                os.path.normpath(pattern))):
+            cmd(['rm', '-rf', file_path.replace('\\', '/')])
+
+
+def cmd(tokens):
+    logging.info(' '.join(tokens))
+    subprocess.call(tokens)
+
+
+def wget(url, sha1):
+    _, _, file_name = url.rpartition('/')
+    target_path = os.path.join(BASE_DIR, file_name)
+    if os.access(target_path, os.F_OK):
+        if hashlib.sha1(open(target_path, 'rb').read()).hexdigest() == sha1:
+            return
+    logging.info('Downloading {}'.format(url))
+    req = urllib.request.urlopen(url)
+    open(target_path, 'wb+').write(req.read())
+
+
+main()
